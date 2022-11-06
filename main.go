e040f64a78d11fd5a500a8865952293fca6c6586
AOrps <aeo34@njit.edu>
2022-11-06 11:45:57 -0500

    talk to me asdfasdfsdf

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	util "github.com/diamondguy43/hackathan/util"
 	"github.com/joho/godotenv"
@@ -31,22 +33,35 @@ func root(w http.ResponseWriter, r *http.Request) {
 		// err := decoder.Decode(&res)
 		// util.Check(err)
 
-		// fmt.Println(r.Body)
-
-		// fmt.Println(res["sDate"])
-
-		var gPost P_REQ
-		err := json.NewDecoder(r.Body).Decode(&gPost)
+		fmt.Println(r.Body)
+		// fmt.Println(r.Body.Read())
+		b, err := io.ReadAll(r.Body)
+		// b, err := ioutil.ReadAll(resp.Body)  Go.1.15 and earlier
 		if err != nil {
-			fmt.Println("asdlmnasdf")
-			log.Fatal(err)
+			log.Fatalln(err)
 		}
+		fmt.Println(string(b))
+
+		params := strings.Split(string(b), "&")
+		fmt.Println(params)
+
+		sDate := strings.Split(params[0], "=")[1]
+		eDate := strings.Split(params[1], "=")[1]
+		loc := strings.Split(params[2], "=")[1]
+
+		// var gPost P_REQ
+
+		// err := json.NewDecoder(r.Body).Decode(&gPost)
+		// if err != nil {
+		// 	fmt.Println("asdlmnasdf")
+		// 	log.Fatal(err)
+		// }
 		defer r.Body.Close()
 
-		fmt.Println(r.Body)
+		// fmt.Println(r.Body)
 
 		// json message decoder
-		image := util.QRKeyGen(gPost.StartDate, gPost.EndDate, gPost.Location)
+		image := util.QRKeyGen(sDate, eDate, loc)
 		w.Write(image)
 		// http.Redirect(w, r, "localhost:9900/qr", http.StatusAccepted)
 	default:
